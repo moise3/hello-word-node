2ac34112776b95cac42c7309af50a94ec76f41ac
demo-m13 <demo-m13@TeamCity>
2020-12-30 06:07:36 +0000

    TeamCity change in 'Hello Word Node' project: runners of 'Build Pack' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -114,7 +114,7 @@ object Build1 : BuildType({
         script {
             name = "Build Image"
             scriptContent = """
-                docker run \
+                sudo docker run \
                   -v /var/run/docker.sock:/var/run/docker.sock \
                   -v ${'$'}PWD/src:/workspace -w /workspace \
                   buildpacksio/pack build mkameni/helloworld:vsaas-pack --builder cloudfoundry/cnb:bionic
